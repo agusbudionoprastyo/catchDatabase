df28662006a6e58b570a73c9ecf441d9489ab2c6
agusbudionoprastyo <agna4605@gmail.com>
2024-06-23 12:33:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ async function pollChanges() {
     try {
         conn = await pool.getConnection();
         const rows = await conn.query(`
-        SELECT orders.order_serial_no, orders.total, 
+        SELECT orders.order_serial_no, orders.total, orders.total_tax,
         REPLACE(REPLACE(REPLACE(GROUP_CONCAT(CONCAT(items.name, " ", CASE WHEN JSON_LENGTH(item_variations) = 0 THEN '' ELSE CONCAT(" ", JSON_EXTRACT(item_variations, '$[*].name')) END, " ", order_items.quantity) SEPARATOR ',  '), '["', 'include '), '"]', ''), '"', '') AS item_names,
         dining_tables.name AS table_name
         FROM orders 
@@ -63,16 +63,25 @@ async function pollChanges() {
 //     }
 // }
 
+// Function to format the subtotal
+function formatCurrency(amount) {
+    return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(amount).replace('Rp', 'Rp');
+}
 
-// Fungsi untuk mengirim permintaan HTTP ke endpoint Google Apps Script
+// Fungsi untuk mengirim permintaan HTTP ke endpoint
 async function sendRequests(rows) {
     for (let row of rows) {
         const orderId = row.order_serial_no;
         const total = row.total;
+        const tax = row.total_tax
         const itemName = row.item_names;
         const tableName = row.table_name;
+        const subtotalCurency = formatCurrency(total);
+        const taxNserviceCurency = formatCurrency(tax);
+        const totalCurency = formatCurrency(parseFloat(total) + parseFloat(tax));
         const url = 'https://wagateway.dafamsemarang.my.id/send-group-message'; // URL baru
-        const payload = { message: `Order ID: ${orderId}, Total: ${total}, Items: ${itemName}, Table: ${tableName}`, id_group: 'your_group_id' }; // Sesuaikan id_group
+        const message = `*Hai canting, ada pesanan baru nih! Nomor Pesanan ${orderId}, Room/Table ${tableName}*\n\n_Item_\n${itemName}\n\n_Subtotal_\n${subtotalCurency}\n\n_Tax & Service_ 21%\n${taxNserviceCurency}\n\n_Total_\n${totalCurency}\n\n_klik tautan berikut untuk mengkonfirmasi pesanan_\nhttps://cantingfood.my.id\n_Thankyou & happy working_`;
+        const payload = { message, id_group: '120363304142052316@g.us' }; // Sesuaikan id_group
 
         try {
             await axios.post(url, new URLSearchParams(payload), {
@@ -87,6 +96,25 @@ async function sendRequests(rows) {
     }
 }
 
+// Fungsi untuk menguji endpoint setiap 1 jam
+async function testEndpoint() {
+    const url = 'https://wagateway.dafamsemarang.my.id/send-group-message'; // URL yang sama dengan yang digunakan di sendRequests
+    const payload = { message: 'API status : running', id_group: '120363304142052316@g.us' }; // Sesuaikan id_group
+
+    try {
+        await axios.post(url, new URLSearchParams(payload), {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+        console.log('Test endpoint berhasil:', payload);
+    } catch (error) {
+        console.error('Error menguji endpoint:', error);
+    }
+}
+
+// Set interval untuk menguji endpoint setiap 1 jam (3600000 ms)
+setInterval(testEndpoint, 3600000);
 
 // Set interval untuk memanggil polling perubahan setiap 5 detik
 setInterval(pollChanges, 5000);
